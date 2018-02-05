8ee5232e3647f7720586d7ee98d0a45aa8e74e4f
HanTaeWoong <gksxodnd007@gmail.com>
2018-02-05 21:41:25 +0900

    auth_server

---
+++ auth_server/src/main/java/com/finder/genie_ai/GenieAiApplication.java
@@ -0,0 +1,22 @@
+package com.finder.genie_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EntityScan(
+		basePackages = { "com.finder.genie_ai.model" },
+		basePackageClasses = {Jsr310JpaConverters.class})
+public class GenieAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenieAiApplication.class, args);
+	}
+
+}
