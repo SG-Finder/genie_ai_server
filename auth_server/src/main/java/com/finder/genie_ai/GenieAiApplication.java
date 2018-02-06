2b0221f2530757cdb2560b342319eaf692559053
codingsquid <gksxodnd007@gmail.com>
2018-02-07 02:52:19 +0900

    announce start class

--- auth_server/src/main/java/com/finder/genie_ai/GenieAiApplication.java
+++ auth_server/src/main/java/com/finder/genie_ai/GenieAiApplication.java
@@ -3,6 +3,8 @@ package com.finder.genie_ai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
@@ -13,7 +15,12 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 @EntityScan(
 		basePackages = { "com.finder.genie_ai.model" },
 		basePackageClasses = {Jsr310JpaConverters.class})
-public class GenieAiApplication {
+public class GenieAiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GenieAiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		//System.getProperties().put("server.port", 8082);
