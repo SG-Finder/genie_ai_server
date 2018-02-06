29999a4f538a55298bf1f1e2c912b2320952a0ce
codingsquid <gksxodnd007@gmail.com>
2018-02-07 04:16:29 +0900

    rollback setting

--- auth_server/src/main/java/com/finder/genie_ai/GenieAiApplication.java
+++ auth_server/src/main/java/com/finder/genie_ai/GenieAiApplication.java
@@ -1,38 +1,22 @@
 package com.finder.genie_ai;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.WebApplicationInitializer;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @EntityScan(
 		basePackages = { "com.finder.genie_ai.model" },
 		basePackageClasses = {Jsr310JpaConverters.class})
-@EnableJpaRepositories(basePackages = "com.finder.genie_ai.dao")
-@EnableJpaAuditing
-public class GenieAiApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return configureApplication(application);
-	}
+public class GenieAiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GenieAiApplication.class, args);
-	}
-
-	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
-		return builder.sources(GenieAiApplication.class).bannerMode(Banner.Mode.OFF);
 	}
 
 }
